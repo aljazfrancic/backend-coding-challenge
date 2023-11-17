842f36068c1966d6689095befb8f4f6fc1934de1
Aljaž <aljaz.francic@gmail.com>
2023-11-17 17:01:55 +0100

    MySQL basic configuration

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,20 @@
 -- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(1, 'field-1');
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+
+drop table if exists movie;
+
+create table movie
+(
+    id          varchar(255) not null,
+    title       varchar(255) not null,
+    year        int,
+    description varchar(10000),
+    primary key (id)
+);
+
+insert into movie (id, title, year, description)
+values ('tt0133093', 'The Matrix', 1999,
+        'When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.'),
+       ('tt0266697', 'Kill Bill: Vol. 1', 2003,
+        'After awakening from a four-year coma, a former assassin wreaks vengeance on the team of assassins who betrayed her.'),
+       ('tt0073486', 'One Flew Over the Cuckoo''s Nest', 1975,
+        'In the Fall of 1963, a Korean War veteran and criminal pleads insanity and is admitted to a mental institution, where he rallies up the scared patients against the tyrannical nurse.');
